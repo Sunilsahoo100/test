91cb789d684b66b9ba1306dde3e2b627abd31e54
amansingh331 <amansingh.nitrkl@gmail.com>
2023-03-05 15:11:01 +0530

    updated

--- practice1.cpp
+++ practice1.cpp
@@ -16,32 +16,9 @@ int main()
     cin>>t;
     while(t--)
     {
-    	cin>>n;
-    	cin>>s;
-    	transform(s.begin(), s.end(), s.begin(), ::tolower);
-    	string ans;
     	
-    	if(s[0]!='m')
-    		cout<<"NO"<<endl;
-    	else
-    	{
-    		ans.push_back('m');
-    		for(int i=1;i<n;i++)
-	    	{
-	    		
-	    		if(s[i]!=s[i-1])
-	    			ans.push_back(s[i]);
-	    		
-	    	}
-    	}
-    	if(ans.size()!=4)
-    		cout<<"NO"<<endl;
-    	else
-    	{
-    		if(ans[0]=='m' && ans[1]=='e' && ans[2]=='o' && ans[3]=='w' )
-	    	cout<<"YES"<<endl;
 	    	else
-	    	cout<<"NO"<<endl;
+	    	cout<<"NOgffgdgdgdgddgdvv"<<endl;
     	}
     	
 	    
